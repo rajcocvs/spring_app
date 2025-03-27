453841c34d97d68887b872fe76109a7415bd104e
rajco <rajcosiva@gmail.com>
2025-03-28 02:07:19 +0800

    controller

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
+  
+    @GetMapping({"/", "/home"})
     public String home(Model model) {
         model.addAttribute("message", "Welcome to My Spring Boot Web Application!");
         return "home";
